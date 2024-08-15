0c2cb491278258b6a4b9c016a01976368a7c2f6f
itottan <itottan@gmail.com>
2024-08-16 01:15:10 +0900

    custom user

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,53 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)  # ユーザー認証、ユーザー管理、パーミッションの制御など、Django アプリケーションでの認証機能をサポートするための包括的なコンポーネントを提供
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """通常のユーザーを作成し、保存する."""
+
+        if not email:
+            raise ValueError('The Email field must be set.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        スーパーユーザーを作成し、保存する.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Manager を objects に設定することで、ユーザーモデルに対してカスタムマネージャーが利用されます。
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
